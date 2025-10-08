095e1acdeeb81e7b9ca346839db72612fbecdd63
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-07 20:25:14 -0400

    its kinda faster ig?

--- hand_recognition/recognition.py
+++ hand_recognition/recognition.py
@@ -6,8 +6,9 @@ class landmarker_and_result:
 
     def __init__(self):
 
-        self.result = mp.tasks.vision.HandLandmarkerResult
+        self.result = None
         self.landmarker = mp.tasks.vision.HandLandmarker
+        
         self.create_land_marker()
 
     def create_land_marker(self):
@@ -31,66 +32,46 @@ class landmarker_and_result:
         self.landmarker = self.landmarker.create_from_options(options)
 
     def detect_async(self, frame):
-
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-
-        self.landmarker.detect_async(mp_image, int(time.time() * 1000))
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
+        try:
+            self.landmarker.detect_async(mp_image, int(time.time() * 1000))
+        except:
+            pass
 
     def close(self):
 
         self.landmarker.close() 
 
-def draw_landmarks_on_image(rgb_image, detection_result: mp.tasks.vision.HandLandmarkerResult):
-
-    try:
-
-        if detection_result.hand_landmarks == 0:
-
-            return rgb_image
-        
-        else:
-
-            # fetch list of landmarks
-            hand_landmarks_list = detection_result.hand_landmarks
-
-            # copy ts image
-            annotated_image = rgb_image.copy()
-
-            # Loop through the detected hands
-            for hand_landmarks in hand_landmarks_list:
-                for landmark in hand_landmarks:
-                    h, w, _ = annotated_image.shape
-                    cx, cy = int(landmark.x * w), int(landmark.y * h)
-                    cv2.circle(annotated_image, (cx, cy), 5, (0, 255, 0), -1)
-
-            return annotated_image
-
-                
-
-
-    except:
-
+def draw_landmarks_on_image(rgb_image, detection_result):
+    if not detection_result:
         return rgb_image
+    hand_landmarks_list = getattr(detection_result, 'hand_landmarks', None)
+    if not hand_landmarks_list:
+        return rgb_image
+    h, w, _ = rgb_image.shape
+    for hand_landmarks in hand_landmarks_list:
+        for landmark in hand_landmarks:
+            cx, cy = int(landmark.x * w), int(landmark.y * h)
+            cv2.circle(rgb_image, (cx, cy), 4, (0, 255, 0), -1)
+    return rgb_image
     
 
 
 def main():
 
     cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
     hand_landmarker = landmarker_and_result()
 
     while True:
-
         # Capture frame
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
 
-        # Detect hands & print results
-
         hand_landmarker.detect_async(frame)
-        print(hand_landmarker.result)
-
-        frame = draw_landmarks_on_image(frame,hand_landmarker.result)
+        frame = draw_landmarks_on_image(frame, hand_landmarker.result)
 
         # Show the frame
         cv2.imshow('frame', frame)
