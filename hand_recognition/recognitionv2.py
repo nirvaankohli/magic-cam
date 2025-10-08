198cbf57055c645820530ac77115f562489a0114
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-07 21:13:20 -0400

    formatted doc

--- hand_recognition/recognitionv2.py
+++ hand_recognition/recognitionv2.py
@@ -1,8 +1,9 @@
-import cv2 
+import cv2
 
 import mediapipe.python.solutions.hands as mp_hands
 import mediapipe.python.solutions.drawing_utils as mp_drawing
 import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
+
 
 def process_frame(frame, hands):
 
@@ -10,31 +11,30 @@ def process_frame(frame, hands):
     results = hands.process(frame_rgb)
 
     if results.multi_hand_landmarks:
-        
+
         for landmarks in results.multi_hand_landmarks:
 
             mp_drawing.draw_landmarks(
-
                 image=frame,
                 landmark_list=landmarks,
                 connections=mp_hands.HAND_CONNECTIONS,
                 landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
                 connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style(),
-            
             )
 
-    return frame
+    return frame, results
+
 
 def main():
 
     cap = cv2.VideoCapture(0)
 
     with mp_hands.Hands(
-
         model_complexity=0,
         max_num_hands=2,
         min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
+        min_tracking_confidence=0.5,
+    ) as hands:
 
         while cap.isOpened():
 
@@ -55,28 +55,22 @@ def main():
                 for landmarks in results.multi_hand_landmarks:
 
                     mp_drawing.draw_landmarks(
-
                         image=frame,
-
                         landmark_list=landmarks,
-                        
                         connections=mp_hands.HAND_CONNECTIONS,
-                        
                         landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
-                        
                         connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style(),
-                    
                     )
 
             cv2.imshow("Magic Cam - Only Hand Recognition - press 'q' to quit", frame)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+
                 break
 
     cap.release()
 
+
 if __name__ == "__main__":
 
     main()
-
