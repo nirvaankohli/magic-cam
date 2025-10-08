77daaa66a95e9f7df4a5b96ef56036e612616fda
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-08 19:21:34 -0400

    basic camera engine!

--- gesture_recognition/background_subtraction.py
+++ gesture_recognition/background_subtraction.py
@@ -1,1 +1,29 @@
-from hand_recognition import recognitionv2 as hrec
+import cv2
+import mediapipe.python.solutions.hands as mp_hands
+
+
+def main():
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+
+        ret, frame = cap.read()
+
+        if not ret:
+
+            break
+
+        frame = cv2.flip(frame, 1)
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        cv2.imshow("Magic Cam - Only Hand Recognition - press 'q' to quit", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+
+            break
+
+
+if __name__ == "__main__":
+    main()
