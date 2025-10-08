15e5981303efd06b0bb301bf05c54979d68326cc
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-07 20:31:35 -0400

    imports

---
+++ hand_recognition/recognitionv2.py
@@ -0,0 +1,5 @@
+import cv2 
+
+import mediapipe.python.solutions.hands as mp_hands
+import mediapipe.python.solutions.drawing_utils as mp_drawing
+import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
