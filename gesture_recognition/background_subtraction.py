c50ab350533730e11d691d67c229e8b57fd49687
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-08 19:47:12 -0400

    got x and y coords

--- gesture_recognition/background_subtraction.py
+++ gesture_recognition/background_subtraction.py
@@ -7,9 +7,10 @@ module_dir = Path(__file__).parent.parent / "hand_recognition"
 sys.path.insert(0, str(module_dir))
 
 
-def get_hand_bbox(landmarks, img_width, img_height):
+def get_hand_bbox(lm, img_width, img_height):
 
-    
+    x_coords = [lm.x * img_width for lm in lm.landmark]
+    y_coords = [lm.y * img_height for lm in lm.landmark]
 
 
 import recognitionv2 as hrec
@@ -41,13 +42,9 @@ def main():
         fr, results = hrec.process_frame(frame, hands)
 
         print(
-
             get_hand_bbox(
-                
                 results.multi_hand_landmarks[0], frame.shape[1], frame.shape[0]
-           
             )
-            
             if results.multi_hand_landmarks
             else "No hands detected"
         )
