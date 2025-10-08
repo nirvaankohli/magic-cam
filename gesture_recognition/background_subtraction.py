a8b284701a1ef787d50b43093b5476dc1d3dda72
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-08 19:42:24 -0400

    wait nvm

--- gesture_recognition/background_subtraction.py
+++ gesture_recognition/background_subtraction.py
@@ -6,6 +6,11 @@ import sys
 module_dir = Path(__file__).parent.parent / "hand_recognition"
 sys.path.insert(0, str(module_dir))
 
+
+def get_hand_bbox(landmarks, img_width, img_height):
+
+
+
 import recognitionv2 as hrec
 
 
@@ -25,7 +30,15 @@ def main():
 
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        frame, results = hrec.process_frame(frame, hands)
+        hands = mp_hands.Hands(
+            model_complexity=0,
+            max_num_hands=2,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5,
+        )
+
+        fr, results = hrec.process_frame(frame, hands)
+
 
         cv2.imshow("Magic Cam - Only Hand Recognition - press 'q' to quit", frame)
 
