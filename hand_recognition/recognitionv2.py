83cecb5536d02427236ba7972a6d96de3d95104b
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-07 20:33:13 -0400

    Integrates hand tracking setup with configurable options

--- hand_recognition/recognitionv2.py
+++ hand_recognition/recognitionv2.py
@@ -7,3 +7,12 @@ import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
 def main():
 
     cap = cv2.VideoCapture(0)
+
+    with mp_hands.Hands(
+
+        model_complexity=0,
+        max_num_hands=2,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+
+        
