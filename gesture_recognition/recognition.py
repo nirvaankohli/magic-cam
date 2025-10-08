edb53a8eb89242696eb0ec3bd8f1af6447d455f5
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-08 08:39:56 -0400

    tried to tell ai smthn i wanted to do did not work lmao

--- gesture_recognition/recognition.py
+++ gesture_recognition/recognition.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 from hand_recognition import recognitionv2 as hrec
 import cv2
+import numpy as np
+import mediapipe.python.solutions.hands as mp_hands
+import mediapipe.python.solutions.drawing_utils as mp_drawing
+import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
+import math
 
 root_dir = Path(__file__).parent.parent
