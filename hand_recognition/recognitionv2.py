5952e9e3ee24f0032b00a4309d2a745c5df889fb
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-07 20:31:58 -0400

    capturing ts video

--- hand_recognition/recognitionv2.py
+++ hand_recognition/recognitionv2.py
@@ -3,3 +3,7 @@ import cv2
 import mediapipe.python.solutions.hands as mp_hands
 import mediapipe.python.solutions.drawing_utils as mp_drawing
 import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
+
+def main():
+
+    cap = cv2.VideoCapture(0)
