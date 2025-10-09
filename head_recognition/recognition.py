1dac2f0b26c369a4820ab714e4388cf696356b16
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-09 17:48:27 -0400

    yo i just did ts

--- head_recognition/recognition.py
+++ head_recognition/recognition.py
@@ -1,2 +1,25 @@
 import cv2
-def main()
+
+
+def main():
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+
+        success, frame = cap.read()
+
+        frame = cv2.flip(frame, 1)
+
+        cv2.imshow("The frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+
+    main()
